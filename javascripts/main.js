5e93d77e709958de9e37f6a07b4c2318ef2cda9d
daclouds <daclouds@gmail.com>
2015-03-04 18:19:53 +0900

    SVG Drawing Animation
    http://tympanus.net/codrops/2013/12/30/svg-drawing-animation/

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,17 @@
-$(document).ready(function(){
-    $('.imgBox li').css({display:'none',opacity:0});
-    $('.imgBox li.act').css({display:'block',opacity:1});
-    $('.dotBox a').click(function(){
-        var imgNum=$('.dotBox a').index(this);
-        $(this).parent().addClass('active').siblings().removeClass();
-        $('.imgBox li.act').animate({opacity:0},1000,function(){$(this).css('display','none')}).removeClass('act');
-        $('.imgBox li').eq(imgNum).css('display','block').animate({opacity:1},1000).addClass('act');
-    });
-});
+
+(function() {
+
+	'use strict';
+
+	$(document).ready(function(){
+	    $('.imgBox li').css({display:'none',opacity:0});
+	    $('.imgBox li.act').css({display:'block',opacity:1});
+	    $('.dotBox a').click(function(){
+	        var imgNum=$('.dotBox a').index(this);
+	        $(this).parent().addClass('active').siblings().removeClass();
+	        $('.imgBox li.act').animate({opacity:0},1000,function(){$(this).css('display','none')}).removeClass('act');
+	        $('.imgBox li').eq(imgNum).css('display','block').animate({opacity:1},1000).addClass('act');
+	    });
+	});
+	
+})();
